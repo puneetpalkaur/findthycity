8157d90027899c47e386bd670b862c105760705e
puneetpalkaur <billingpuneet.13@gmail.com>
2020-09-09 01:24:48 -0400

    working pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'FindThatCity';
+export class AppComponent implements OnInit{
+  title = 'FindThyCity';
+  httpClient: any;
+  states: any;
+  cityName = '';
+  cities = [];
+  constructor(httpClient: HttpClient) {
+    this.httpClient = httpClient;
+  }
+  ngOnInit(){
+    this.httpClient.get('assets/cities.json').subscribe(data => {
+
+      if (data){
+        this.states = Object.entries(data);
+        this.states.forEach((state) => {
+          state[1].forEach(city => this.cities.push(city + ', ' + state[0]));
+        });
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { FilterPipe } from './filter/filter.pipe';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
